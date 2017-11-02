eea3e4cb6418d35ae9bede0569e79d7d784c7642
hazem osama ibrahim <hazem3500@gmail.com>
2017-11-02 20:22:23 +0200

    changed Id's in MainActivity

--- app/src/main/java/com/fci/e_com/MainActivity.java
+++ app/src/main/java/com/fci/e_com/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_inbox) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_grades) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_news) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_schedule) {
+
+        } else if (id == R.id.nav_logout) {
 
         }
 
